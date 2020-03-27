71de7b27427fa898e4737e69c8ff6995151ce0ce
GianmarcoMontanari <giammont@live.it>
2020-03-27 14:04:00 +0100

    struttura di partenza

---
+++ main.js
@@ -0,0 +1,5 @@
+// Creo una versione dell’EX del biglietto del treno ricca con un bell’output dinamico;
+// Usare eventi sui 2 bottoni principali, e sviluppare le logiche che sottendono;
+// “Genera” mi calcolerà il prezzo del biglietto a partire dai dati inseriti negli input
+// e mi draà l’output in pagina, nel modo migliore possibile;
+// “Annulla” mi fa tornare allo stato iniziale con output pulito
